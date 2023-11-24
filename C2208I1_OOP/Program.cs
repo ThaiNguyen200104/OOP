75a59978e657daaf53c06c69c2427f3ff18db789
Thái Nguyễn <quangthai0997@gmail.com>
2023-11-24 14:43:06 +0700

    Create Class Student, Auto Property, Objecr by Constructor, Object Initializer

--- C2208I1_OOP/Program.cs
+++ C2208I1_OOP/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using C2208I1_OOP;
+
+#region code cũ
+//// Object by constructor (dựa vào constructor)
+//Student stu = new Student(1, "Nguyen", true, new DateTime(2004, 01, 20));
+//Student stu1 = new(2, "Le", true, new DateTime(2004, 01, 20));
+#endregion
+
+#region code mới
+// Object initializer (không cần contructor)
+Student stu3 = new()
+{
+    Id = 3,
+    Fullname = "Quang",
+    Gender = true,
+    Dob = DateTime.Now
+};
+#endregion
+
+
+
---
+++ C2208I1_OOP/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace C2208I1_OOP;
+internal class Student
+{
+    // Properties: C# có cách viết riêng - không dùng getter & setter, khác với Java
+    // Style cũ thường dùng cho Mobile (do Java cũ)
+    #region style code cũ
+    //// Các field
+    //int? id;
+    //string? fullname;
+    //bool? gender;
+    //DateTime? dob;
+
+    //public int? Id { get => id; set => id = value; }
+    //public string? Fullname { get => fullname; set => fullname = value; }
+    //public bool? Gender { get => gender; set => gender = value; }
+    //public DateTime? Dob { get => dob; set => dob = value; }
+    #endregion
+
+    #region style code mới
+    // Vừa là field vừa là auto property
+    public int? Id { get; set; }
+    public string? Fullname { get; set; }
+    public bool? Gender { get; set; }
+    public DateTime? Dob { get; set; }
+    #endregion
+
+    //public Student() { }
+    //public Student(int? id, string? fullname, bool? gender, DateTime? dob)
+    //{
+    //    Id = id;
+    //    Fullname = fullname;
+    //    Gender = gender;
+    //    Dob = dob;
+    //}
+
+    public override string ToString()
+    {
+        return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Fullname)}={Fullname}, {nameof(Gender)}={Gender.ToString()}, {nameof(Dob)}={Dob.ToString()}}}";
+    }
+}
